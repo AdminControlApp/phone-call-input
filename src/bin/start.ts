e02c6e480d086dd482b69de364e7ba2de4d86cd9
Leon Si <contact@leonzalion.com>
2022-04-11 09:36:52 -0400

    fix: don't use top level await

--- src/bin/start.ts
+++ src/bin/start.ts
@@ -4,15 +4,17 @@ import { makeCall } from '~/utils/call.js';
 import { startNgrokServer } from '~/utils/ngrok.js';
 import { startAppServer } from '~/utils/server.js';
 
-try {
-	await startAppServer();
-	const ngrokServerUrl = await startNgrokServer();
-	await makeCall({ ngrokServerUrl });
-} catch (error: unknown) {
-	const err = error as Error;
-	console.error('There was an error.');
-	// Replace all numeric characters with asterisks to prevent leaking
-	// the password
-	console.error('Name:', err.name.replace(/\d/g, '*'));
-	console.error('Message:', err.message.replace(/\d/g, '*'));
-}
+(async () => {
+	try {
+		await startAppServer();
+		const ngrokServerUrl = await startNgrokServer();
+		await makeCall({ ngrokServerUrl });
+	} catch (error: unknown) {
+		const err = error as Error;
+		console.error('There was an error.');
+		// Replace all numeric characters with asterisks to prevent leaking
+		// the password
+		console.error('Name:', err.name.replace(/\d/g, '*'));
+		console.error('Message:', err.message.replace(/\d/g, '*'));
+	}
+})();
